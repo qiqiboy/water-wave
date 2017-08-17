e45ed915d7e2702a749130d46f33d186bc966672
qiqiboy <imqiqiboy@gmail.com>
2017-08-17 17:05:17 +0800

    增加对触屏更好的支持
    Signed-off-by: qiqiboy <imqiqiboy@gmail.com>

---
+++ src/WaterWave.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { requestAnimationFrame, cancelAnimationFrame } from 'raf-dom';
+import * as events from './events';
+
+class Water extends Component {
+    componentDidMount() {
+        const canvasParent = this.refs.canvas.parentNode;
+        const position = window.getComputedStyle(canvasParent, null).position;
+
+        if (position === 'static') {
+            canvasParent.style.position = 'relative';
+        }
+
+        canvasParent.classList.add('water-wave-target');
+
+        Object.keys(events.event2code)
+            .forEach(type => {
+                canvasParent.addEventListener(type, this, false);
+            });
+    }
+
+    componentWillUnmount() {
+        const canvasParent = this.refs.canvas.parentNode;
+
+        cancelAnimationFrame(this.timer);
+        canvasParent.classList.remove('water-wave-container');
+
+        Object.keys(events.event2code)
+            .forEach(type => {
+                canvasParent.removeEventListener(type, this, false);
+            });
+    }
+
+    handleEvent(ev) {
+        const code = events.getCode(ev.type);
+        const group = events.getGroup(ev.type);
+
+        switch (code) {
+            case 0:
+                if (!this.eventGroup) {
+                    this.eventGroup = group;
+                }
+            case 2:
+                //确保前后一致的事件类型
+                if (this.eventGroup === group) {
+                    const _ev = events.format(ev);
+
+                    const { pageX, pageY } = _ev;
+                    const now = Date.now();
+
+                    clearTimeout(this.clearTimer);
+
+                    if (code === 0 && !this.startState) {
+                        this.startState = {
+                            pageX,
+                            pageY,
+                            now
+                        }
+                    }
+
+                    if (code === 2 && this.startState) {
+                        if (now - this.startState.now < 400 &&
+                            Math.abs(pageX - this.startState.pageX) < 3 &&
+                            Math.abs(pageY - this.startState.pageY) < 3) {
+                            this.createWave(_ev);
+                        }
+                    }
+
+                    if (code === 2) {
+                        this.clearEvent();
+                    }
+                }
+                break;
+            case 3:
+                this.clearEvent();
+                break;
+        }
+    }
+
+    clearEvent() {
+        this.clearTimer = setTimeout(() => {
+            delete this.eventGroup;
+            delete this.startState;
+        }, 350);
+    }
+
+    createWave = ev => {
+        if (!this.refs.canvas.parentNode.disabled) {
+            const dpr = window.devicePixelRatio || 1;
+            const { top, left, width, height } = this.refs.canvas.parentNode.getBoundingClientRect();
+            const { clientX, clientY } = ev;
+            const x = clientX - left;
+            const y = clientY - top;
+            const canvas = this.refs.canvas;
+            const ctx = canvas.getContext('2d');
+            const { duration } = this.props;
+
+            canvas.width = width * dpr;
+            canvas.height = height * dpr;
+
+            ctx.scale(dpr, dpr);
+
+            const startTime = Date.now();
+            const run = () => {
+                const now = Date.now();
+                const offset = now - startTime;
+
+                cancelAnimationFrame(this.timer);
+                ctx.clearRect(0, 0, width, height);
+
+                if (offset < duration) {
+                    this.draw(ctx, x, y, offset / duration * Math.max(width, height), Math.min(.3, 1 - offset / duration));
+
+                    this.timer = requestAnimationFrame(run);
+                }
+            }
+
+            run();
+        }
+    }
+
+    draw(ctx, x, y, radius, opacity) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = this.props.color;
+        ctx.globalAlpha = opacity;
+        ctx.fill();
+    }
+
+    render() {
+        return (
+            <canvas ref="canvas" className="water-wave-canvas"></canvas>
+        );
+    }
+
+    static defaultProps = {
+        duration: 500,
+        color: '#fff'
+    }
+
+    static propTypes = {
+        duration: PropTypes.number,
+        color: PropTypes.string
+    }
+}
+
+export default Water;
---
+++ src/events.js
@@ -0,0 +1,39 @@
+export const event2code = {
+    touchstart: 0,
+    pointerdown: 0,
+    mousedown: 0,
+
+    touchmove: 1,
+    pointermove: 1,
+    mousemove: 1,
+
+    touchend: 2,
+    pointerup: 2,
+    mouseup: 2,
+
+    touchcancel: 3,
+    pointercancel: 3
+}
+
+export const getCode = type =>
+    event2code[type.toLowerCase()];
+
+export const getGroup = type =>
+    type.toLowerCase().match(/(touch|pointer|mouse)/i)[0];
+
+export const format = ev => {
+    const group = getGroup(ev.type);
+
+    if (group === 'touch') {
+        const { clientX, clientY, pageX, pageY } = ev.changedTouches.item(0);
+
+        return Object.assign(ev, {
+            clientX,
+            clientY,
+            pageX,
+            pageY
+        });
+    }
+
+    return ev;
+}
